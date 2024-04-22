f9c11da3245da261e21305902de60d7737206e0d
Bass913 <bastiendikiadi@outlook.fr>
2024-04-22 17:04:08 +0200

    test v9

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more. Test v8.0
+        Click on the Vite and React logos to learn more. Test v9
       </p>
     </>
   )
