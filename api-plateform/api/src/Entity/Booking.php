fa3a6f77d1bd27489b7d405ba7c7d80be77dfa62
Taslima <taslimaahamedmze@gmail.com>
2024-05-08 23:40:19 +0200

    Entity booking

---
+++ api-plateform/api/src/Entity/Booking.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\BookingRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: BookingRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read-booking']]
+)]
+class Booking
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Groups(['read-booking'])]
+    #[ORM\Column(length: 255)]
+    private ?string $startDate = null;
+
+    #[Groups(['read-booking'])]
+    #[ORM\Column(length: 255)]
+    private ?string $endDate = null;
+
+    #[Groups(['read-booking'])]
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStartDate(): ?string
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(string $startDate): static
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?string
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(string $endDate): static
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
