71b0f9eab60c6241db10352fb192eef85d0404a1
Rafael93150 <tavaresrafael93@gmail.com>
2024-05-16 11:16:42 +0200

    add company to userfixture

--- api-plateform/api/src/DataFixtures/UserFixtures.php
+++ api-plateform/api/src/DataFixtures/UserFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Repository\CompanyRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -10,21 +11,24 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class UserFixtures extends Fixture
 {
     private UserPasswordHasherInterface $passwordHasher;
+    private CompanyRepository $companyRepository;
 
-    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    public function __construct(UserPasswordHasherInterface $passwordHasher, CompanyRepository $companyRepository)
     {
+        $this->companyRepository = $companyRepository;
         $this->passwordHasher = $passwordHasher;
     }
 
     public function load(ObjectManager $manager)
     {
-        // Création d'un utilisateur de test
+        $companies = $this->companyRepository->findAll();
+
         $user = new User();
         $user->setFirstname('John');
         $user->setLastname('Doe');
         $user->setEmail('user@user.fr');
         $user->setRoles(['ROLE_USER']);
-        // Encodage du mot de passe
+        $user->setCompany($companies[0]);
         $plaintextPassword = 'test';
         $hashedPassword = $this->passwordHasher->hashPassword(
             $user,
