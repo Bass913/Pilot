9e2fc4d59f159ce7f37e2645d179223c617c9b3b
Rafael93150 <tavaresrafael93@gmail.com>
2024-06-07 16:11:41 +0200

    add services and speciality selector at searchbar

--- front/src/components/SearchBar.jsx
+++ front/src/components/SearchBar.jsx
@@ -1,17 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button.jsx";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import apiService from "../services/apiService";
 
-const SearchBar = ({ initialGarageSearch, initialAddressSearch }) => {
+const SearchBar = ({ initialGarageSearch = "", initialAddressSearch = "" }) => {
 	const { t } = useTranslation();
 	const [garageSearch, setGarageSearch] = useState(initialGarageSearch);
 	const [addressSearch, setAddressSearch] = useState(initialAddressSearch);
+	const [services, setServices] = useState([]);
+	const [servicesToShow, setServicesToShow] = useState([]);
+	const [specialities, setSpecialities] = useState([]);
+	const [specialitiesToShow, setSpecialitiesToShow] = useState([]);
+	const [showServices, setShowServices] = useState(false);
 	const navigate = useNavigate();
+
+	const fetchServices = async () => {
+		const response = await apiService.getServices();
+		setServices(response.data["hydra:member"]);
+		setServicesToShow(response.data["hydra:member"]);
+	};
+
+	const fetchSpecialities = async () => {
+		const response = await apiService.getSpecialities();
+		setSpecialities(response.data["hydra:member"]);
+		setSpecialitiesToShow(response.data["hydra:member"]);
+	};
+
+	const filterServices = (search) => {
+		const servicesFiltered = services.filter((service) =>
+			service.name.toLowerCase().includes(search.toLowerCase())
+		);
+		setServicesToShow(servicesFiltered);
+	};
+
+	const filterSpecialities = (search) => {
+		const specialitiesFiltered = specialities.filter((speciality) =>
+			speciality.name.toLowerCase().includes(search.toLowerCase())
+		);
+		setSpecialitiesToShow(specialitiesFiltered);
+	};
+
+	useEffect(() => {
+		fetchServices();
+		fetchSpecialities();
+	}, []);
 
 	const handleGarageSearchChange = (e) => {
 		setGarageSearch(e.target.value);
+		filterServices(e.target.value);
+		filterSpecialities(e.target.value);
 	};
 	const handleAddressSearchChange = (e) => {
 		setAddressSearch(e.target.value);
@@ -26,8 +65,52 @@ const SearchBar = ({ initialGarageSearch, initialAddressSearch }) => {
 	return (
 		<div
 			onSubmit={handleSubmit}
-			className="flex items-center justify-center w-full md:w-150 lg:w-175 bg-white rounded-lg p-2 h-20"
+			className="flex items-center justify-center w-full md:w-150 lg:w-175 bg-white rounded-lg p-2 h-20 relative"
 		>
+			{showServices && (
+				<div className="absolute top-5 left-0 h-52 w-full flex text-gray-600 transform translate-y-16 bg-white rounded-lg overflow-hidden shadow-md">
+					{servicesToShow.length > 0 && (
+						<div className="w-5/12 h-full py-2">
+							<p className="text-sm text-gray-800 font-normal py-2 px-3 underline">
+								{t("services")}
+							</p>
+							<ul className="flex flex-col w-full h-full overflow-y-auto align-start">
+								{servicesToShow.map((service) => (
+									<li
+										key={service.id}
+										className="text-sm text-gray-600 font-normal py-2 px-4 hover:bg-gray-100 w-full cursor-pointer"
+										onClick={() =>
+											setGarageSearch(service.name)
+										}
+									>
+										{service.name}
+									</li>
+								))}
+							</ul>
+						</div>
+					)}
+					{specialitiesToShow.length > 0 && (
+						<div className="w-7/12 h-full py-2">
+							<p className="text-sm text-gray-800 font-normal py-2 px-3 underline">
+								{t("specialities")}
+							</p>
+							<ul className="flex flex-col w-full h-full overflow-y-auto align-start">
+								{specialitiesToShow.map((speciality) => (
+									<li
+										key={speciality.id}
+										className="text-sm text-gray-600 font-normal py-2 px-4 hover:bg-gray-100 w-full cursor-pointer"
+										onClick={() =>
+											setGarageSearch(speciality.name)
+										}
+									>
+										{speciality.name}
+									</li>
+								))}
+							</ul>
+						</div>
+					)}
+				</div>
+			)}
 			<div className="w-6 ml-2">
 				<MagnifyingGlassIcon className="w-full text-gray-600" />
 			</div>
@@ -40,6 +123,12 @@ const SearchBar = ({ initialGarageSearch, initialAddressSearch }) => {
 					name="search"
 					onChange={handleGarageSearchChange}
 					value={garageSearch}
+					onFocus={() => setShowServices(true)}
+					onBlur={() => {
+						setTimeout(() => {
+							setShowServices(false);
+						}, 100);
+					}}
 				/>
 				<hr className="w-0 h-10 border-r border-gray-400 mx-2" />
 				<input
