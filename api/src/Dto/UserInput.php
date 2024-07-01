a31bfd59706a5d60d61d7d79c51a584edc982423
Taslima-Ahamed-Mze <taslimaahamedmze@gmail.com>
2024-07-02 00:43:47 +0200

    Sécu company post patch et delete

--- api/src/Dto/UserInput.php
+++ api/src/Dto/UserInput.php
@@ -23,11 +23,6 @@ class UserInput
 
     #[Groups(['user:create'])]
     #[Assert\NotBlank]
-    #[Assert\Length(min: 8)]
-    public ?string $password = null;
-
-    #[Groups(['user:create'])]
-    #[Assert\NotBlank]
     public ?string $phone = null;
 
     #[Groups(['user:create'])]
