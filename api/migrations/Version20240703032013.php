3479346d9024b2e4c5af898e31c0c078ba25263b
Bass913 <bastiendikiadi@outlook.fr>
2024-07-03 05:21:25 +0200

    old data removed

--- api/migrations/Version20240703032013.php
+++ api/migrations/Version20240703032013.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240703023352 extends AbstractMigration
+final class Version20240703032013 extends AbstractMigration
 {
     public function getDescription(): string
     {
